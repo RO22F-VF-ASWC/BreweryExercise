9457fe0f3ffc6858108594bec2517f61cf777446
Peter Levinsky <pele@easj.dk>
2022-03-11 11:15:34 +0100

    Add project files.

---
+++ BreweryConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using BreweryLib.ThreadCollections;
+
+namespace BreweryConsoleApp
+{
+    class Program
+    {
+        public static TraceSource TRACE;
+        static void Main(string[] args)
+        {
+            //BoundedBuffer<String> ss = new BoundedBuffer<string>(); // derved default 1.000.000
+            //BoundedBuffer<String> ss2 = new BoundedBuffer<string>(100);
+
+            TRACE = new TraceSource("Brewery");
+            TRACE.Listeners.Add(
+                new TextWriterTraceListener(
+                    new StreamWriter(@"c:\Logfiler\Brewery.txt")));
+            TRACE.Listeners.Add(new ConsoleTraceListener());
+
+
+            TRACE.Switch = new SourceSwitch("Brewery", "all");
+
+            BreweryWorker worker = new BreweryWorker();
+            worker.Start();
+
+            Console.WriteLine("Simulation finished - look in logfile");
+            Console.ReadLine();
+        }
+    }
+}
